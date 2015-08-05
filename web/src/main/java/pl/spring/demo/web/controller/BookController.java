3e8c34a90af644fe5c8988e62a005fec3962482d
Marzena <marzena.gorlas@gmail.com>
2015-08-05 10:41:45 +0200

    created table of books and button delete and method delete

--- web/src/main/java/pl/spring/demo/web/controller/BookController.java
+++ web/src/main/java/pl/spring/demo/web/controller/BookController.java
@@ -2,8 +2,11 @@ package pl.spring.demo.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import pl.spring.demo.service.BookService;
 import pl.spring.demo.to.BookTo;
 
@@ -12,13 +15,25 @@ import java.util.Map;
 
 @Controller
 public class BookController {
-    @Autowired
-    private BookService bookService;
+	@Autowired
+	private BookService bookService;
 
-    @RequestMapping(value = "/books", method = RequestMethod.GET)
-    public String bookList(Map<String, Object> params) {
-        final List<BookTo> allBooks = bookService.findAllBooks();
-        params.put("books", allBooks);
-        return "bookList";
-    }
+	@RequestMapping(value = "/books", method = RequestMethod.GET)
+	public String bookList(Map<String, Object> params) {
+		final List<BookTo> allBooks = bookService.findAllBooks();
+		params.put("books", allBooks);
+		return "bookList";
+	}
+
+	@RequestMapping(value = "/book-delete/{bookId}", method = RequestMethod.GET)
+	public String deleteBook(@PathVariable Long bookId, Map<String, Object> params) {
+		final List<BookTo> allBooks = bookService.findAllBooks();
+		BookTo book = null;
+		for (BookTo bookTo : allBooks) {
+			if(bookTo.getId() == bookId) book = bookTo;
+		}
+		bookService.deleteBook(book.getId());
+		params.put("bookDelete", book.getTitle());
+		return "bookList";
+	}
 }
