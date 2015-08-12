1a3efe39de86c2eaf18c0f94627e32d37c339381
Marzena <marzena.gorlas@gmail.com>
2015-08-12 15:04:46 +0200

    test

--- spring-data-jpa/src/main/java/pl/spring/demo/dao/LibraryDao.java
+++
@@ -1,9 +0,0 @@
-package pl.spring.demo.dao;
-
-import java.util.List;
-
-import pl.spring.demo.entity.LibraryEntity;
-
-public interface LibraryDao extends Dao<LibraryEntity, Long> {
-	List<LibraryEntity> findLibraryByName(String Name);
-}
