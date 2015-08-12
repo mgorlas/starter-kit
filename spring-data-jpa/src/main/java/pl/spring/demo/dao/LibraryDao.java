58cc3f13e04ebea2a1cee525c28ff06e394f2d92
Marzena <marzena.gorlas@gmail.com>
2015-08-12 14:53:38 +0200

    Created library dao and library repository

--- spring-data-jpa/src/main/java/pl/spring/demo/dao/LibraryDao.java
+++ spring-data-jpa/src/main/java/pl/spring/demo/dao/LibraryDao.java
@@ -1,9 +1,9 @@
 package pl.spring.demo.dao;
 
-import pl.spring.demo.entity.LibraryEntity;
-
 import java.util.List;
 
+import pl.spring.demo.entity.LibraryEntity;
+
 public interface LibraryDao extends Dao<LibraryEntity, Long> {
-    List<LibraryEntity> findLibraryByName(String Name);
+	List<LibraryEntity> findLibraryByName(String Name);
 }
--- spring-data-jpa/src/main/java/pl/spring/demo/dao/impl/LibraryDaoImpl.java
+++ spring-data-jpa/src/main/java/pl/spring/demo/dao/impl/LibraryDaoImpl.java
@@ -1,21 +1,20 @@
 package pl.spring.demo.dao.impl;
 
-
+import java.util.List;
 
 import javax.persistence.TypedQuery;
 
 import pl.spring.demo.dao.LibraryDao;
 import pl.spring.demo.entity.LibraryEntity;
 
-import java.util.List;
+public class LibraryDaoImpl extends AbstractDao<LibraryEntity, Long>implements LibraryDao {
 
-public class LibraryDaoImpl extends AbstractDao<LibraryEntity, Long> implements LibraryDao {
-
-    @Override
-    public List<LibraryEntity> findLibraryByName(String name) {
-        TypedQuery<LibraryEntity> query = entityManager.createQuery(
-                "select library from LibraryEntity library where upper(library.name) like concat(upper(:name), '%')", LibraryEntity.class);
-        query.setParameter("name", name);
-        return query.getResultList();
-    }
+	@Override
+	public List<LibraryEntity> findLibraryByName(String name) {
+		TypedQuery<LibraryEntity> query = entityManager.createQuery(
+				"select library from LibraryEntity library where upper(library.name) like concat(upper(:name), '%')",
+				LibraryEntity.class);
+		query.setParameter("name", name);
+		return query.getResultList();
+	}
 }
