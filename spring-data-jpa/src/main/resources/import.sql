f5537930e34521336875db97309e1fe5d9197cd5
Marzena <marzena.gorlas@gmail.com>
2015-08-21 10:57:02 +0200

    Fixed names tables

--- spring-data-jpa/src/main/resources/import.sql
+++ spring-data-jpa/src/main/resources/import.sql
@@ -1,3 +1,12 @@
-insert into book (id, title, authors) values (1, 'Pierwsza książka', 'Jan Kowalski');
-insert into book (id, title, authors) values (2, 'Druga książka', 'Zbigniew Nowak');
-insert into book (id, title, authors) values (3, 'Trzecia książka', 'Janusz Jankowski');
+insert into book (id, title) values (1, 'Pierwsza książka');
+insert into book (id, title) values (2, 'Druga książka');
+insert into book (id, title) values (3, 'Trzecia książka');
+
+insert into author (id, first_name, last_name) values (1, 'Zbigniew', 'Nowak');
+insert into author (id, first_name, last_name) values (2, 'Jan', ' Kowalski');
+insert into author (id, first_name, last_name) values (3, 'Janusz', ' Jankowski');
+
+insert into book_author (book_id, author_id) values (1, 1);
+insert into book_author (book_id, author_id) values (2, 1);
+insert into book_author (book_id, author_id) values (2, 2);
+insert into book_author (book_id, author_id) values (3, 3);
